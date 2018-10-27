c6670e100dc772d0d7613b3a5b4270de1614a34c
Mind <15130077@st.hcmuaf.edu.vn>
2018-10-27 21:08:39 +0700

    change name models

--- shop_api/main_api/admin.py
+++ shop_api/main_api/admin.py
@@ -3,3 +3,7 @@ from .models import *
 # Register your models here.
 admin.site.register(Categories)
 admin.site.register(Products)
+admin.site.register(Order)
+admin.site.register(Order_items)
+admin.site.register(Customers)
+admin.site.register(Dilivery_address)
--- shop_api/main_api/models.py
+++ shop_api/main_api/models.py
@@ -5,7 +5,6 @@ from django.db import models
 
 from django.core.validators import RegexValidator
 
-phone_regex = RegexValidator(regex = r'^\+?1?\d{9, 15}$', message = "Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
 class Logins(models.Model):
     username = models.CharField(max_length = 100)
     password = models.CharField(max_length = 50)
@@ -18,10 +17,8 @@ class Logins(models.Model):
 class Customers(models.Model):
     forename = models.CharField(max_length = 100)
     surename = models.CharField(max_length = 100)
-    add1 = models.CharField(max_length = 200)
-    add2 = models.CharField(max_length = 200)
-    add3 = models.CharField(max_length = 200)
-    phone = models.CharField(validators = [phone_regex], max_length = 14, blank = True)
+    add =  models.ForeignKey("Dilivery_address", on_delete=models.CASCADE)
+    phone = models.CharField( max_length = 14, blank = True)
     email = models.EmailField()
     postcode = models.IntegerField(default = 0)
     registered = models.BooleanField(default = False)
@@ -29,40 +26,36 @@ class Customers(models.Model):
     def __str__(self):
         return self.forename
 
-class Dilivery_addresses (models.Model):
+class Dilivery_address(models.Model):
     forename = models.CharField(max_length = 100)
     surename = models.CharField(max_length = 100)
     add1 = models.CharField(max_length = 200)
-    add2 = models.CharField(max_length = 200)
-    add3 = models.CharField(max_length = 200)
-    phone = models.CharField(validators = [phone_regex], max_length = 14, blank = True)
-    email = models.EmailField()
+    phone = models.CharField(  max_length = 14, blank = True)
     postcode = models.IntegerField(default = 0)
 
     def __str__(self):
         return self.forename
 
 
-class Others(models.Model):
+class Order(models.Model):
     customer = models.ForeignKey(Customers, on_delete = models.CASCADE)
     registered = models.BooleanField(default = False)
     delivery = models.ForeignKey(
-    Dilivery_addresses, on_delete = models.CASCADE)
+    Dilivery_address, on_delete = models.CASCADE)
     payment_type = models.CharField(max_length = 20)
-    date = models.DateTimeField()
-    status = models.CharField(max_length = 50)
-    session = models.CharField(max_length = 100)
+    date = models.DateTimeField( auto_now=False ,auto_now_add=True)
+    status = models.CharField(max_length = 50,default='waiting')
+    session = models.CharField(max_length = 100,default ='00000')
     total = models.IntegerField(default = 0)
 
     def __str__(self):
         return self.status
 
 
-class Other_items(models.Model):
-    other = models.ForeignKey(Others, on_delete = models.CASCADE)
+class Order_items(models.Model):
+    order = models.ForeignKey(Order, on_delete = models.CASCADE)
     product_id = models.ForeignKey('Products', on_delete = models.Case)
     quanlity = models.IntegerField(default = 0)
-
     def __str__(self):
         return self.quanlity
 
